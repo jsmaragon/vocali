892907c98f4e8365836126e798f4415b69df1f98
Jose M Aragon <josemarianoa@hotmail.com>
2024-03-05 06:30:44 +0100

    added scheduler

---
+++ InvoxMedical/Scheduler/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NCrontab.Scheduler.Extensions;
+using System.IO;
+using System.Threading.Tasks;
+
+class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var hostBuilder = new HostBuilder()
+            .ConfigureAppConfiguration((hostContext, configBuilder) =>
+            {
+                configBuilder.SetBasePath(Directory.GetCurrentDirectory());
+                configBuilder.AddJsonFile("appsettings.json", optional: true);
+                configBuilder.AddJsonFile(
+                    $"appsettings.{hostContext.HostingEnvironment.EnvironmentName}.json",
+                    optional: true);
+                configBuilder.AddEnvironmentVariables();
+            })
+            .ConfigureLogging((hostContext, configLogging) =>
+            {
+                configLogging.AddConfiguration(hostContext.Configuration.GetSection("Logging"));
+                configLogging.AddConsole();
+            })
+            .ConfigureServices((hostContext, services) =>
+            {
+                services.AddScheduler();
+                services.AddScoped<IHostedService, TimedHostedService>();
+                // Here goes your internal application dependencies
+                // like EntityFramework context, worker, endpoint, etc.
+            });
+
+        await hostBuilder.RunConsoleAsync();
+    }
+}
